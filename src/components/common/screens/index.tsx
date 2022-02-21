968678ecfe4056857197fbe27a77f8c5206b570b
Azmeer02 <syedazmeerhaider@gmail.com>
2022-02-21 19:19:28 +0500

    app completed

--- src/components/common/screens/index.tsx
+++ src/components/common/screens/index.tsx
@@ -27,22 +27,18 @@ const Dashboard: React.FC<InputProps> = ({ data, id }: any) => {
   const [orders, setOrders] = useState<any>(null);
   const [allOrders, setAllOrders] = useState<any>(null);
 
+  /* For Specific User */
   useEffect(() => {
     const userData = async () => {
       const fs = fireStore;
       const docRef = doc(fs, "User", `${orderId}`);
       const docSnap = await getDoc(docRef);
-
-      if (docSnap.exists()) {
-        // console.log("Document data:", docSnap.data());
-        setOrders(docSnap.data());
-      } else {
-        console.log("No such document!");
-      }
+      setOrders(docSnap.data());
     };
     userData();
   }, [orderId]);
 
+  /* For Whole Collection */
   useEffect(() => {
     const allData = async () => {
       const fs = fireStore;
@@ -51,7 +47,6 @@ const Dashboard: React.FC<InputProps> = ({ data, id }: any) => {
         const foods: any = [];
         querySnapshot.forEach((doc) => {
           foods.push(doc.data());
-          // console.log("data=", doc.data());
         });
         setAllOrders(foods);
       });
@@ -68,40 +63,6 @@ const Dashboard: React.FC<InputProps> = ({ data, id }: any) => {
         </div>
       </div>
       <div className="dashboard">
-        {/* <Paper elevation={8} className="order-paper">
-          <Card className="card" title={orders?.Name} bordered={true}>
-            <p>
-              <b className="order-label">Restaurant Name: </b>
-              <u>
-                <b className="order-list">{orders?.RestaurantName}</b>
-              </u>
-            </p>
-            <p>
-              <b className="order-label">{orders?.Name} Order: </b>
-              <u>
-                <b className="order-list">{orders?.ItemOrder}</b>
-              </u>
-            </p>
-            <p>
-              <b className="order-label">Total Order Cost: </b>
-              <u>
-                <b className="order-list">{orders?.Price}</b>
-              </u>
-            </p>
-            <p>
-              <b className="order-label">Amount {orders?.Name} have: </b>
-              <u>
-                <b className="order-list">{orders?.Amount}</b>
-              </u>
-            </p>
-            <p>
-              <b className="order-label">Amount To Return: </b>
-              <u>
-                <b className="order-list">{orders?.TotalAmount}</b>
-              </u>
-            </p>
-          </Card>
-        </Paper> */}
         {allOrders &&
           Object.entries(allOrders).map((item: any) => {
             return (
