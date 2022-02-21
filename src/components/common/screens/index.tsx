30a6e31281d658cb4c8dc50aeaa44a31ff4cb4ba
Azmeer02 <syedazmeerhaider@gmail.com>
2022-02-21 17:40:29 +0500

    completed

--- src/components/common/screens/index.tsx
+++ src/components/common/screens/index.tsx
@@ -5,6 +5,7 @@ import { Paper } from "@mui/material";
 import "firebase/firestore";
 import fireStore from "../Config/firebase";
 import { doc, getDoc } from "firebase/firestore";
+import { onSnapshot, query, collection } from "firebase/firestore";
 
 interface Props {
   name: string;
@@ -23,7 +24,9 @@ type InputProps = {
 
 const Dashboard: React.FC<InputProps> = ({ data, id }: any) => {
   const orderId = new URLSearchParams(window.location.search).get("orderId");
+  const [orders, setOrders] = useState<any>(null);
   const [allOrders, setAllOrders] = useState<any>(null);
+
   useEffect(() => {
     const userData = async () => {
       const fs = fireStore;
@@ -31,14 +34,30 @@ const Dashboard: React.FC<InputProps> = ({ data, id }: any) => {
       const docSnap = await getDoc(docRef);
 
       if (docSnap.exists()) {
-        console.log("Document data:", docSnap.data());
-        setAllOrders(docSnap.data());
+        // console.log("Document data:", docSnap.data());
+        setOrders(docSnap.data());
       } else {
         console.log("No such document!");
       }
     };
     userData();
   }, [orderId]);
+
+  useEffect(() => {
+    const allData = async () => {
+      const fs = fireStore;
+      const q = query(collection(fs, "User"));
+      const unsubscribe = await onSnapshot(q, (querySnapshot) => {
+        const foods: any = [];
+        querySnapshot.forEach((doc) => {
+          foods.push(doc.data());
+          // console.log("data=", doc.data());
+        });
+        setAllOrders(foods);
+      });
+    };
+    allData();
+  }, []);
 
   return (
     <>
@@ -49,40 +68,81 @@ const Dashboard: React.FC<InputProps> = ({ data, id }: any) => {
         </div>
       </div>
       <div className="dashboard">
-        <Paper elevation={8} className="order-paper">
-          <Card className="card" title={allOrders?.Name} bordered={true}>
+        {/* <Paper elevation={8} className="order-paper">
+          <Card className="card" title={orders?.Name} bordered={true}>
             <p>
               <b className="order-label">Restaurant Name: </b>
               <u>
-                <b className="order-list">{allOrders?.RestaurantName}</b>
+                <b className="order-list">{orders?.RestaurantName}</b>
               </u>
             </p>
             <p>
-              <b className="order-label">{allOrders?.Name} Order: </b>
+              <b className="order-label">{orders?.Name} Order: </b>
               <u>
-                <b className="order-list">{allOrders?.ItemOrder}</b>
+                <b className="order-list">{orders?.ItemOrder}</b>
               </u>
             </p>
             <p>
               <b className="order-label">Total Order Cost: </b>
               <u>
-                <b className="order-list">{allOrders?.Price}</b>
+                <b className="order-list">{orders?.Price}</b>
               </u>
             </p>
             <p>
-              <b className="order-label">Amount {allOrders?.Name} have: </b>
+              <b className="order-label">Amount {orders?.Name} have: </b>
               <u>
-                <b className="order-list">{allOrders?.Amount}</b>
+                <b className="order-list">{orders?.Amount}</b>
               </u>
             </p>
             <p>
               <b className="order-label">Amount To Return: </b>
               <u>
-                <b className="order-list">{allOrders?.TotalAmount}</b>
+                <b className="order-list">{orders?.TotalAmount}</b>
               </u>
             </p>
           </Card>
-        </Paper>
+        </Paper> */}
+        {allOrders &&
+          Object.entries(allOrders).map((item: any) => {
+            return (
+              <Paper elevation={8} className="order-paper" key={item}>
+                <Card className="card" title={item?.[1]?.Name} bordered={true}>
+                  <p>
+                    <b className="order-label">Restaurant Name: </b>
+                    <u>
+                      <b className="order-list">{item?.[1]?.RestaurantName}</b>
+                    </u>
+                  </p>
+                  <p>
+                    <b className="order-label">{item?.[1]?.Name} Order: </b>
+                    <u>
+                      <b className="order-list">{item?.[1]?.ItemOrder}</b>
+                    </u>
+                  </p>
+                  <p>
+                    <b className="order-label">Total Order Cost: </b>
+                    <u>
+                      <b className="order-list">{item?.[1]?.Price}</b>
+                    </u>
+                  </p>
+                  <p>
+                    <b className="order-label">
+                      Amount {item?.[1]?.Name} have:{" "}
+                    </b>
+                    <u>
+                      <b className="order-list">{item?.[1]?.Amount}</b>
+                    </u>
+                  </p>
+                  <p>
+                    <b className="order-label">Amount To Return: </b>
+                    <u>
+                      <b className="order-list">{item?.[1]?.TotalAmount}</b>
+                    </u>
+                  </p>
+                </Card>
+              </Paper>
+            );
+          })}
       </div>
     </>
   );
